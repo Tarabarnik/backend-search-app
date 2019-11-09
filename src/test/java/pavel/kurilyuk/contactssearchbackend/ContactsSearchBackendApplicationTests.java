f2cff646c87181a144987546021b35dd20405982
Taranarnik <pavelkurilyuk97@gmail.com>
2019-11-09 19:40:52 +0200

    added request tests

--- src/test/java/pavel/kurilyuk/contactssearchbackend/ContactsSearchBackendApplicationTests.java
+++ src/test/java/pavel/kurilyuk/contactssearchbackend/ContactsSearchBackendApplicationTests.java
@@ -1,16 +1,25 @@
 package pavel.kurilyuk.contactssearchbackend;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pavel.kurilyuk.contactssearchbackend.entity.Contact;
 import pavel.kurilyuk.contactssearchbackend.service.ContactService;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @SpringBootTest
 class ContactsSearchBackendApplicationTests {
@@ -36,5 +45,30 @@ class ContactsSearchBackendApplicationTests {
         Contact dbVova = contactService.add(vova);
         assertEquals(vova.getName(), dbVova.getName());
         assertNotNull(dbVova.getId());
+    }
+
+    @Test
+    void emptyPatternReceive500() throws IOException {
+        HttpUriRequest request = new HttpGet("http://localhost:8080/hello/contacts?nameFilter=");
+
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+
+        assertThat(
+                httpResponse.getStatusLine().getStatusCode(),
+                equalTo(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+    }
+
+    @Test
+    void getRequestReturnsJson() throws IOException {
+        String jsonMimeType = "application/json";
+        Contact vova = new Contact("Vova");
+        contactService.add(vova);
+        HttpUriRequest request = new HttpGet("http://localhost:8080/hello/contacts?nameFilter="
+                + startsWithNumberRegex);
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+        assertEquals(jsonMimeType, mimeType);
     }
 }
